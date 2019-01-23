ce887d043487fa970ee66e4ecffcc37beb1f8e4b
Tyler Patton <tylerpatton@tylers-mbp.attlocal.net>
2019-01-23 00:14:20 -0600

    changes by TDawg:p

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,8 @@
-// Write your Javascript code.
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 56) {
+        document.getElementById("navi").setAttribute('style', 'background-color:rgba(0,0,0,0.9)!important');
+    }
+    else{
+        document.getElementById("navi").setAttribute('style', 'background-color:transparent!important');
+    }
+}
